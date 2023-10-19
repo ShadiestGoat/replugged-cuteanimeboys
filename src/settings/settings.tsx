566ad63930dd40c1ecb9827a209befdf9f98dfc7
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2023-10-19 17:04:33 +0100

    Lint >:(

--- src/settings/settings.tsx
+++ src/settings/settings.tsx
@@ -4,7 +4,7 @@ const { Flex, SwitchItem } = components;
 
 export function Settings(): React.ReactElement {
   return (
-    <Flex style={{gap: "2vh"}} direction={Flex.Direction.VERTICAL}>
+    <Flex style={{ gap: "2vh" }} direction={Flex.Direction.VERTICAL}>
       <SwitchItem
         {...util.useSetting(cfg, "includeNSFW")}
         note="If disabled, images marked as NSFW will be skipped">
